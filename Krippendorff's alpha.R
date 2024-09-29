336de1e6c65759dea7e8d6f38d24bd325117ca71
Gedee70 <x6w70@students.keele.ac.uk>
2024-09-29 17:53:42 +0100

    Add files via upload

---
+++ Krippendorff's alpha.R
@@ -0,0 +1,10 @@
+library(krippendorffsalpha)
+nominal=matrix(c(1,1,2,1,2,2,4,5,4,1,6,7,8,1,1,12,12,1,1,2,2,2,4,10,3,3,8,9,10,1,8,4,8,2,2,7,1,2,3,
+                 10,8,1,13,1,1,2,3,11,1,10,10,1,2,8,1,4,3,2,1,1,1,
+                 1,1,2,1,2,2,4,5,4,1,6,7,8,1,1,12,12,1,3,2,2,2,4,10,3,3,8,9,10,1,8,4,8,3,2,7,1,2,3,
+                 10,8,1,13,1,1,2,3,11,1,10,10,1,2,8,1,4,3,2,1,1,1),61,2)
+fit.nom=krippendorffs.alpha(nominal, level="nominal", confint = TRUE, method = "customary", verbose = TRUE, control=list(parallel=FALSE))
+summary(fit.nom)
+confint(fit.nom)
+plot(fit.nom, xlim=c(0.8,1), xlab="Bootstrap Estimates", main="Interrater Agreement across 13 levels", density=FALSE)
+plot(fit.nom, xlim=c(0,1), xlab="Bootstrap Estimates", main="Interrater Agreement across 13 levels", density=FALSE)
